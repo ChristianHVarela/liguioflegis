38f808a39f59e3ba5966e3a0dfd463f40ec3de21
ChristianVarela <christian_varela@outlook.com.br>
2023-04-04 23:53:46 -0300

    fix: task in progress

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import { router } from './routes/routes';
 
-const server = express()
-
+const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(router);
 
 server.listen(5000, () => {
     console.log("SERVER IS FUN IN PÒRT 5000!");
-})
+});
